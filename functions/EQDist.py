338b017ca05d719113ac5e4c73c9afd71e8e5037
Silvio Schwarz <vio.schwarz89@gmail.com>
2019-01-25 11:34:28 +0100

    initial deployment

---
+++ functions/EQDist.py
@@ -0,0 +1,65 @@
+import numpy as np
+from scipy.spatial.distance import cdist
+
+def GeoDistance(lat1, lon1, lat2, lon2, unit):
+    radlat1 = np.pi * lat1/180
+    radlat2 = np.pi * lat2/180
+    theta = lon1-lon2
+    radtheta = np.pi * theta/180
+    dist = np.sin(radlat1) * np.sin(radlat2) + np.cos(radlat1) * np.cos(radlat2) * np.cos(radtheta);
+    dist = np.arccos(dist)
+    dist = dist * 180/np.pi
+    dist = dist * 60 * 1.1515
+
+    if unit=="K":
+            dist = dist * 1.609344
+    if unit=="N":
+            dist = dist * 0.8684
+    return dist
+
+def EQDistances(points,EQDEPTH,Strike,Dip,Length,Width,DeltaL,DeltaW):
+#TODO: DeltaL, DeltaW
+
+    dip = np.radians(-Dip)
+    strike = np.radians(Strike-90)
+
+    cosDip, cosStrike, sinDip, sinStrike = np.cos(dip), np.cos(strike), np.sin(dip), np.sin(strike)
+
+    rotationStrike = np.array([[cosStrike, -sinStrike, 0],
+                               [sinStrike, cosStrike, 0],
+                               [0,0,1]]
+                             )
+
+
+    rotationDip = np.array([[cosDip, 0, -sinDip],
+                           [0, 1, 0],
+                           [sinDip,0,cosDip]]
+                          )
+
+    #DISTANCE CALCULATIONS
+    repi = np.sqrt(np.sum(points**2))
+    rhyp = np.sqrt(repi**2+EQDEPTH**2)
+
+    eqDimensions = np.array([Width/2,Length/2,0])
+    eqDimensionsSurface = eqDimensions * np.array([np.cos(Dip),1,1])
+
+    ### RJB
+    points = np.array(points).dot(rotationStrike)
+    diff = np.abs(points) - eqDimensionsSurface
+
+    rjb = np.sqrt((((np.abs(diff) + diff)/2)**2).sum())
+    ### RRUP
+    points = (np.array(points)+np.array([0,0,EQDEPTH])).dot(rotationDip)
+    diff = np.abs(points) - eqDimensions
+
+    rrup = np.sqrt((((np.abs(diff) + diff)/2)**2).sum())
+
+    #rell = cdist(points, np.array([eqDimensions*np.array([-1,1,1]),
+    #                            eqDimensions*np.array([-1,-1,1])])).mean()
+
+    #rz = cdist(points, np.array([eqDimensions*np.array([1,1,1]),
+    #                            eqDimensions*np.array([-1,1,1]),
+    #                            eqDimensions*np.array([-1,-1,1]),
+    #                            eqDimensions*np.array([1,-1,1])])).mean()
+
+    return np.array([repi,rhyp,rjb,rrup])#,rell,rz)
